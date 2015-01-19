0dfde5faaba662a5313f766ee358a1e65bde2107
Władysław Bodzek <wladek@wikia-inc.com>
2015-01-19 16:02:13 +0100

    Covnert spaces to tabs

--- dist/weppy.d.ts
+++ dist/weppy.d.ts
@@ -1,47 +1,47 @@
 interface WeppyNamespace {
-    timer: WeppyNamespaceTimer;
-    namespace(root:string, path?:string): WeppyNamespace;
-    into(subpath:string): WeppyNamespace;
-    count(name:string, value?:number, annotations?:WeppyContext): void;
-    store(name:string, value?:number, annotations?:WeppyContext): void;
-    setOptions(options:WeppySettings): void;
-    sendPagePerformance(): boolean;
+	timer: WeppyNamespaceTimer;
+	namespace(root:string, path?:string): WeppyNamespace;
+	into(subpath:string): WeppyNamespace;
+	count(name:string, value?:number, annotations?:WeppyContext): void;
+	store(name:string, value?:number, annotations?:WeppyContext): void;
+	setOptions(options:WeppySettings): void;
+	sendPagePerformance(): boolean;
 }
 
 interface WeppyNamespaceTimer {
-    send(name:string, duration:number, annotations?:WeppyContext): void;
-    start(name:string, annotations?:WeppyContext): WeppyTimer;
-    stop(name:string, annotations?:WeppyContext): void;
-    annotate(name:string, annotations:WeppyContext): void;
-    time(name:string, action:any, scope:any, args:any, annotations?:WeppyContext): any;
-    timeSync(name:string, action:any, scope:any, args:any, annotations?:WeppyContext): any;
-    wrap(name:string, action:any, scope:any, annotations?:WeppyContext): () => any;
-    mark(name:string, annotations?:any): void;
+	send(name:string, duration:number, annotations?:WeppyContext): void;
+	start(name:string, annotations?:WeppyContext): WeppyTimer;
+	stop(name:string, annotations?:WeppyContext): void;
+	annotate(name:string, annotations:WeppyContext): void;
+	time(name:string, action:any, scope:any, args:any, annotations?:WeppyContext): any;
+	timeSync(name:string, action:any, scope:any, args:any, annotations?:WeppyContext): any;
+	wrap(name:string, action:any, scope:any, annotations?:WeppyContext): () => any;
+	mark(name:string, annotations?:any): void;
 }
 
 interface WeppyTimer {
-    stop(annotations?:WeppyContext): void;
-    annotate(annotations:WeppyContext): void;
+	stop(annotations?:WeppyContext): void;
+	annotate(annotations:WeppyContext): void;
 }
 
 interface WeppyContext {
-    [s:string]: any;
+	[s:string]: any;
 }
 
 interface WeppySettings {
-    host?: string;
-    transport?: any;
-    active?: boolean;
-    sample?: number;
-    aggregationInterval?: number;
-    maxInterval?: number;
-    decimalPrecision?: number;
-    page?: string;
-    context?: {};
+	host?: string;
+	transport?: any;
+	active?: boolean;
+	sample?: number;
+	aggregationInterval?: number;
+	maxInterval?: number;
+	decimalPrecision?: number;
+	page?: string;
+	context?: {};
 }
 
 interface Window {
-    Weppy: WeppyNamespace;
+	Weppy: WeppyNamespace;
 }
 
 declare var Weppy:WeppyNamespace;
